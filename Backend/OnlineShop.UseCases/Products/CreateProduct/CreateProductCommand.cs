48581c663af4f6bc342688aa870a803ca77311d5
Egor Doroshkevich <egor.doroshkevich@saritasa.com>
2023-08-11 09:36:52 +0500

    Add use case of creating product OS-3

---
+++ Backend/OnlineShop.UseCases/Products/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using OnlineShop.Domain.Enums;
+
+namespace OnlineShop.UseCases.Products.CreateProduct;
+
+/// <summary>
+/// Creates product.
+/// </summary>
+public record CreateProductCommand : IRequest<int>
+{
+    /// <summary>
+    /// Product's name.
+    /// </summary>
+    required public string Name { get; init; }
+
+    /// <summary>
+    /// Product's description.
+    /// </summary>
+    required public string Description { get; init; }
+
+    /// <summary>
+    /// Product's price.
+    /// </summary>
+    required public decimal Price { get; init; }
+
+    /// <summary>
+    /// Category.
+    /// </summary>
+    required public Category Category { get; init; }
+
+    /// <summary>
+    /// Seller's id.
+    /// </summary>
+    required public int SellerId { get; init; }
+}
