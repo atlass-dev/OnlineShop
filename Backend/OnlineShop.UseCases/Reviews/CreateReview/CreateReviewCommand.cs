3329a1737bfcfc4c431e046b5aeaf258a7fb9d44
Egor Doroshkevich <egor.doroshkevich@saritasa.com>
2023-08-11 10:56:49 +0500

    Add review creation use case OS-3

---
+++ Backend/OnlineShop.UseCases/Reviews/CreateReview/CreateReviewCommand.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+
+namespace OnlineShop.UseCases.Reviews.CreateReview;
+
+/// <summary>
+/// Creates review.
+/// </summary>
+public record CreateReviewCommand : IRequest<int>
+{
+    /// <summary>
+    /// Product id.
+    /// </summary>
+    required public int ProductId { get; init; }
+
+    /// <summary>
+    /// Reviewer id.
+    /// </summary>
+    required public int ReviewerId { get; init; }
+
+    /// <summary>
+    /// Rating.
+    /// </summary>
+    required public int Rating { get; init; }
+
+    /// <summary>
+    /// Description.
+    /// </summary>
+    required public string Description { get; init; }
+}
---
+++ Backend/OnlineShop.UseCases/Reviews/CreateReview/CreateReviewCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using OnlineShop.Domain.Entities;
+using OnlineShop.Infrastructure.Abstractions.Database;
+
+namespace OnlineShop.UseCases.Reviews.CreateReview;
+
+/// <summary>
+/// Handler for <see cref="CreateReviewCommand"/>.
+/// </summary>
+public class CreateReviewCommandHandler : IRequestHandler<CreateReviewCommand, int>
+{
+    private readonly IAppDbContext dbContext;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public CreateReviewCommandHandler(IAppDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    /// <inheritdoc/>
+    public async Task<int> Handle(CreateReviewCommand request, CancellationToken cancellationToken)
+    {
+        var review = new Review
+        {
+            ProductId = request.ProductId,
+            Rating = request.Rating,
+            ReviewerId = request.ReviewerId,
+            Description = request.Description
+        };
+
+        await dbContext.Reviews.AddAsync(review, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return review.Id;
+    }
+}
